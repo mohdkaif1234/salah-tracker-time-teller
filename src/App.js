7da280ff7560ef367b1e55f5f3304ada45984172
mohdkaif1234 <mkinnocent738@gmail.com>
2025-05-24 11:34:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SalahTracker from './Components/SalahTracker';
+import PrayerStatus from './Components/PrayerStatus'; // Import PrayerStatus component
+import { PrayerStatusProvider } from './Components/PrayerStatusContext';
+import "./App.css";
+
+const App = () => {
+  return (
+    <PrayerStatusProvider>
+      <div>
+        <h1 className='heading'>Salah Time Teller and Tracker</h1>
+       
+        <SalahTracker />
+        <PrayerStatus /> {/* Include PrayerStatus component */}
+      </div>
+    </PrayerStatusProvider>
+  );
+};
+
+export default App;
